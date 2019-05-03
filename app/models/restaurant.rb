9be2d9eca5ce2a9b4fc6367ce886dbdb84db9e8d
github username <github email address>
2019-05-03 11:35:23 -0400

    welp

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,30 @@
 class Restaurant
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self 
   end
+
+  def self.all
+    @@all
+  end 
+
+  def reviews
+    Review.all.select { |rev| rev.restaurant == self }
+  end 
+
+  def customers
+    #self.reviews.collect { |rev| rev.customer }
+    customer_reviews = Review.all.select { |rev| rev.restaurant == self } 
+    customer_reviews.map { |rev| rev.customers } 
+    
+  end 
+
+  def self.find_by_name(name)
+    @@all.find { |rev| rev.name == name }
+  end 
 
 end
