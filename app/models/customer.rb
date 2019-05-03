89b31fd18d2bac3d936bfbfe94a3d69617cf3cfe
Blake Stoner <stonerblake@gmail.com>
2019-05-03 11:31:55 -0400

    CRUSHED IT

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,50 @@
+#require 'pry'
+
 class Customer
   attr_reader :first_name, :last_name
+
+@@all = []
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @all << self
   end
 
   def full_name
-    "#{first_name} #{last_name}"
+  "#{first_name} #{last_name}"
+  end
+
+  def add_review(restaurant, content, rating)
+  Review.new(self, restaurant, content, rating)
+  end
+
+  def num_reviews
+    reviews.count
+  end
+
+  def restaurants
+    reviews.collect(&:restaurant).uniq
+      #or map?
+  end
+
+#  binding.pry
+
+#############################
+
+  def self.find_by_name(name)
+  all.find { |fname| fname.full_name == name }
+  end
+
+  def self.find_all_by_first_name(first_name)
+  all.select { |first| first.first_name == first_name }
+  end
+
+  def self.all_names
+  all.map(&:full_names)
+  end
+
+    def self.all
+      @all
   end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,35 @@
 class Restaurant
   attr_reader :name
 
+@@all = []
+
   def initialize(name)
     @name = name
+    @all << self
   end
 
+  def customers
+  reviewed.map(&:customer).uniq
+  end
+
+  def reviews
+  Review.all.select { |reviews| reviews.restaurant == self }
+  end
+
+  def average_star_rating
+  review.map(&:rating).count / review.length
+  end
+
+  def longest_review
+  review.max_by { |lreview| lreview.content.length }
+    review.lenth
+  end
+
+  def self.find_by_name(name)
+      all.find { |location| location.name == name }
+    end
+
+    def self.all
+    @@all
+  end
 end
