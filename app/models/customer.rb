f643192bfcb954330896296955b1422634a9fb47
Preston Elliott <preston.elliott24@gmail.com>
2019-05-03 11:31:25 -0400

    Final

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,53 @@
 class Customer
   attr_reader :first_name, :last_name
 
+  @@customer = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@customer << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.all
+    @@customer
+  end
+
+  def add_review(restaurant, content, rating)
+    Review.new(restaurant, content, rating, self)
+  end
+
+  def num_reviews
+    Review.all.select { |review| review.customer == self }.length
+  end
+
+  def self.find_by_name(first_name, last_name)
+    Customer.all.find { |x| x.first_name == first_name && x.last_name == last_name }
+  end
+
+  def self.find_all_by_first_name(first_name)
+    Customer.all.select { |x| x.first_name == first_name }
+  end
+
+  # COME BACK TO THIS ONE
+  # def self.all_names
+  #   Customer.all.
+
+  # end
+
+  # def restaurants(customer)
+  #   customer_reviews = Review.all.select do |review| 
+  #     review.customer == customer 
+  #   end
+  # end
+
+
+
+
+
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,39 @@
 class Restaurant
   attr_reader :name
 
+  @@restaurant = []
+
   def initialize(name)
     @name = name
+    @@restaurant << self
   end
+
+  def self.all
+    @@restaurant
+  end
+
+  def reviews
+    Review.all.select { |review| review.restaurant == self }
+  end
+
+  def average_star_rating
+    review_sum = reviews.sum { |review| review.rating }
+    review_count = reviews.count
+    review_avg = ((review_sum.to_f) / (review_count)) 
+  end
+
+  def self.find_by_name(name)
+    Restaurant.all.find { |x| x.name == name }
+  end
+
+  # COME BACK TO THIS ONE
+  # def customers
+  #   reviews.select { |review| review.customer }.uniq!
+  # end
+
+  # COME BACK TO THIS ONE
+  # def longest_review
+  #   reviews.find { |x| x.content.size.max }
+  # end
 
 end
